c4efe34e52050cc454cae8501f94732a13576765
Emam Uddin <emamskingdom@gmail.com>
2025-04-04 22:55:10 +0600

    added my campaign table

--- src/pages/MyCampaign.jsx
+++ src/pages/MyCampaign.jsx
@@ -1,10 +1,47 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import CampaignTable from "../components/CampaignTable";
 
 const MyCampaign = () => {
+    const {user}=useContext(AuthContext)
+    const [myCampaign,setMyCampaign]=useState([])
+    useEffect(()=>{
+        fetch(`http://localhost:5000/my-campaign/${user?.email}`)
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data)
+            setMyCampaign(data)
+        })
+    },[])
    
     return (
-        <div>
-          
-        </div>
+        <div className="overflow-x-auto">
+            <h1>My Campaign</h1>
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Campaign Info</th>
+              <th>Campaign Type</th>
+              <th>Deadline</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {
+              myCampaign.map(campaign => <CampaignTable campaign={campaign}></CampaignTable> )
+            }
+            
+          </tbody>
+         
+        </table>
+      </div>
     );
 };
 
