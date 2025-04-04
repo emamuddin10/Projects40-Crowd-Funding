38972b091ead671f7934f4fc80c3644f1d8222d8
Emam Uddin <emamskingdom@gmail.com>
2025-04-04 22:16:21 +0600

    all campaign page are added

--- src/pages/AllCampaign.jsx
+++ src/pages/AllCampaign.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import CampaignsCard from "../components/CampaignsCard";
+import CampaignTable from "../components/CampaignTable";
 
 const AllCampaign = () => {
   const loadedCampaigns = useLoaderData();
 
   return (
-    <div>
-      <h1 className="text-center text-5xl text-green-800 py-5">Our Campaign</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-11/12 mx-auto my-10">
-        {loadedCampaigns.map((campaign) => (
-          <CampaignsCard campaign={campaign}></CampaignsCard>
-        ))}
-      </div>
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <th>Campaign Info</th>
+            <th>Campaign Type</th>
+            <th>Deadline</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          {
+            loadedCampaigns.map(campaign => <CampaignTable campaign={campaign}></CampaignTable> )
+          }
+          
+        </tbody>
+       
+      </table>
     </div>
   );
 };
